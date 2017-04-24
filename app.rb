3ac0293ab9eb032b246e5ce47062fbc99931024c
Adam Snow <adamjohnsnow@icloud.com>
2017-04-24 16:47:49 +0100

    add first erb index

--- app.rb
+++ app.rb
@@ -16,6 +16,11 @@ get '/number_test' do
   "10001"
 end
 
-get '/cats' do
+get '/better_cats' do
   send_file 'cats.html'
+end
+
+get '/cats' do
+  @cat_name = ["Amigo", "Oscar", "Viking", "*chika chika* Slim Shady"].sample
+  erb(:index)
 end
